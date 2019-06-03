fbfae23ac08fe3569ec13de1958cf1bc74376c0b
gh1031 <1051421758@qq.com>
2019-06-03 19:56:34 +0800

    feat: HMR at development

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './index.less';
 import 'antd/dist/antd.css';
 
-if (module.hot) {
+if (module.hot && process.env.NODE_ENV === 'development') {
   module.hot.accept();
 }
 ReactDom.render(<App />, document.getElementById('app'));
