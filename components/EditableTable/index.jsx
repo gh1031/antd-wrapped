bed5e4644343bcfdf8b69513a94a9cec3c81b73c
guanhao <guanhao@guanhaodeMacBook-Pro.local>
2019-07-11 16:35:50 +0800

    chore: change file extension

---
+++ components/EditableTable/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const EditableTable = () => {
+  return <p>editable table</p>
+}
+
+EditableTable.propTypes = {
+    
+}
+EditableTable.defaultProps = {
+    
+}
+export default EditableTable;
